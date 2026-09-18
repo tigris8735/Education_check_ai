519e9305aa593383037e0796274229e4ecb16f2b
tigris8735 <den0208@bk.ru>
2026-09-18 10:21:50 +0300

    йййй

--- backend/app/services/user_service/router.py
+++ backend/app/services/user_service/router.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_db
-from user_service import service as user_service
+from app.services.user_service import service as user_service
 from models import User
 from schemas import UserOut, UserUpdate
 from app.shared.permissions import get_current_user
